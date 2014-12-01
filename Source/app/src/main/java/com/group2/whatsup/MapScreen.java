6962234e7c90d59686242be0cb520e44dd25b9d8
Keith Scheuerman <this@keithscheuerman.com>
2014-12-01 12:32:51 -0500

    It's a Christmas miracle!
    The map has shown up.

--- Source/app/src/main/java/com/group2/whatsup/MapScreen.java
+++ Source/app/src/main/java/com/group2/whatsup/MapScreen.java
@@ -10,10 +10,38 @@ import android.widget.Button;
 import com.group2.whatsup.Interop.WUBaseActivity;
 
 
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
+import android.app.Activity;
+import android.os.Bundle;
+
+
 public class MapScreen extends WUBaseActivity {
 
     private Button _EventButton;
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_screen);
+
+        // Get a handle to the Map Fragment
+        GoogleMap map = ((MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map)).getMap();
+
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
+    }
+
+    /*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,8 +55,8 @@ public class MapScreen extends WUBaseActivity {
                 changeIntent();
             }
         });
-        */
-    }
+
+    }*/
 
     public void changeIntent()
     {
